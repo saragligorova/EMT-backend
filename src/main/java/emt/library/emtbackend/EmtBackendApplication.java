5c85fcff50269025562e98728fa33b95160c9ee7
saragligorova <saragligorova9@gmail.com>
2023-03-21 12:52:36 +0100

    init

---
+++ src/main/java/emt/library/emtbackend/EmtBackendApplication.java
@@ -0,0 +1,13 @@
+package emt.library.emtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/emt/library/emtbackend/EmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package emt.library.emtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
